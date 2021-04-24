448456888d6ac468afbdbac2be55c9417a87ae10
isroScientist <67408817+isroScientist@users.noreply.github.com>
2021-04-24 15:50:49 +0530

    Added some Art and movement

---
+++ Ludum Dare 2021/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 moveAmount = moveInput.normalized * moveSpeed * Time.deltaTime;
+        transform.position += (Vector3)moveAmount;
+    }
+}
