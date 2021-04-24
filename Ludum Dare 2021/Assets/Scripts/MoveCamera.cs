12f827dc12b2cf08221155af4cc6398585d2a3d0
isroScientist <67408817+isroScientist@users.noreply.github.com>
2021-04-24 19:42:39 +0530

    Added walls + better cam follow

---
+++ Ludum Dare 2021/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    #region Variables
+    #endregion
+
+    #region unity methods
+
+    public float movingSpeed = 10f;
+
+    void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime * movingSpeed;
+    }
+            #endregion
+}
